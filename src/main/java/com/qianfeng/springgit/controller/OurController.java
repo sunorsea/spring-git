d0544b5b0f0daa0b78c15ef896023c8f9c400ce6
ronghaibo <1874012876@qq.com>
2019-06-07 12:24:56 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/springgit/controller/OurController.java
@@ -0,0 +1,14 @@
+package com.qianfeng.springgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author rhb
+ * @version 1.0
+ * @Date 2019/6/7
+ */
+@Controller
+@RequestMapping("our")
+public class OurController {
+}
